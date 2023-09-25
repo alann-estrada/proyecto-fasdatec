5b938da6a6baa4fad5fce64a2f9fa8472a754a33
alann-estrada <142933070+alann-estrada@users.noreply.github.com>
2023-09-25 11:17:22 -0600

    Modificaciones a todo el css y reemplazado por sass moderno

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
-import './css/estilos.css'
-import { Header } from './pages/header';
-import { Main } from './pages/main';
+import "./css/estilos.css";
+import { Header } from "./pages/header";
+import { Main } from "./pages/main";
 
 function App() {
   return (
     <div>
-      <Header/>
-      <Main/>
+      <Header />
+      <Main />
     </div>
   );
 }
